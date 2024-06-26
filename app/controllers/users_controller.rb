72717144ff4e6903e246693f1608115f6d7286d4
Kohei Wakita <wakita.904@gmail.com>
2024-06-26 19:42:53 +0900

    add user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,19 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "welcome to Home Stock Manager"
+      redirect_to @user
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
