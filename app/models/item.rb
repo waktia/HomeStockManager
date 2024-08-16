220e4bbbe485de2d517c40bcdafae857997a67b7
Kohei Wakita <wakita.904@gmail.com>
2024-08-16 16:45:32 +0900

    hot fix weekly limit pg

--- app/models/item.rb
+++ app/models/item.rb
@@ -22,10 +22,10 @@ class Item < ApplicationRecord
 
   if Rails.env.production?
     scope :weekly_limit, -> {
-      where("DATE(COALESCE(updated_at, created_at)) + INTERVAL '1 day' * days <= ?", Date.today + 7 )
+      where("DATE(COALESCE(updated_at, created_at) + INTERVAL '1 day' * round((days * stock / 100.0)::numeric, -1)) <= DATE(?)", Date.today + 7)
     }
     scope :monthly_limit, -> {
-      where("DATE(COALESCE(updated_at, created_at)) + INTERVAL '1 day' * days <= ?", Date.today + 31 )
+      where("DATE(COALESCE(updated_at, created_at) + INTERVAL '1 day' * round((days * stock / 100.0)::numeric, -1)) <= DATE(?)", Date.today + 31)
     }
 
   else
